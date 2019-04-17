a4efb48c89dddb22a38ee4445ce5a40f7ff80e48
EX2OUTCODING\eleandro <emarin@outcoding.com>
2019-04-17 16:30:47 -0600

    no message

--- lazy.js
+++ lazy.js
@@ -13,35 +13,79 @@ function loadCSSImages(config) {
             for (var index = 0; index < entries.length; index++) {
                 var entry = entries[index];
 
-                if(entry.isIntersecting){
-                    console.log("loading ", entry.target.dataset.class);
-                    entry.target.className += " " + entry.target.dataset.class;
+                if (entry.isIntersecting) {
+                    entry.target.className += " " + entry.target.getAttribute('data-class');;
                     observer.unobserve(entry.target);
                 }
-                
+
             }
-            
+
         }, options);
 
         for (var index = 0; index < lazyClassImages.length; index++) {
             var image = lazyClassImages[index];
             observer.observe(image);
-            
+
         }
-    }
-    else{
+    } else {
         for (var index = 0; index < lazyClassImages.length; index++) {
             var image = lazyClassImages[index];
-            image.className += " " + image.dataset.class;
+            image.className += " " + image.getAttribute('data-class');;
         }
     }
-    
+
 
 }
+
+function loadImageTags(config) {
+    var lazyImages = document.querySelectorAll(".nhs_Lazy");
+
+
+
+    var options = {
+        rootMargin: config.margin,
+        root: null
+    };
+
+    var observer = new IntersectionObserver(function (entries) {
+        for (var index = 0; index < entries.length; index++) {
+            var entry = entries[index];
+
+            if (entry.isIntersecting) {
+                entry.target.src += " " + entry.target.getAttribute('data-src');;
+                observer.unobserve(entry.target);
+            }
+
+        }
+    }, options)
+
+    lazyImages.forEach(img => observer.observe(img));
+}
+
+function loadPictures(config) {
+    var pictures = document.querySelectorAll("picture > source");
+    let lazyImageObserver =
+        new IntersectionObserver(function (entries, observer) {
+            entries.forEach(function (entry) {
+                if (entry.isIntersecting) {
+                    let lazyImage = entry.target;
+                    lazyImage.srcset = lazyImage.dataset.srcset;
+                    // lazyImage.nextElementSibling.srcset = lazyImage.dataset.srcset;
+                    lazyImageObserver.unobserve(lazyImage);
+                }
+            });
+        });
+
+    pictures.forEach(function (lazyImage) {
+        lazyImageObserver.observe(lazyImage);
+    });
+}
+
 var config = {
     css: true,
-    images: true, 
-    margin: "50px"
+    images: true,
+    pictures: true,
+    margin: "150px"
 }
 
 function lazy(config) {
@@ -50,7 +94,11 @@ function lazy(config) {
     }
 
     if (config.images) {
-        //loadImages(config);
+        loadImageTags(config);
+    }
+
+    if (config.pictures) {
+        loadPictures(config);
     }
 }
 
